0014fd9995258efe5f8c458e56b3f6e547cde55a
Aram <102629190+aramxva@users.noreply.github.com>
2022-05-28 23:24:48 +0300

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,9 @@
+document.getElementById("wrapper").style.display = "none";
+
+setTimeout(()=>{
+
+    document.getElementById("loading").remove();
+    document.getElementById("wrapper").style.display = "block";
+
+
+},1500);
